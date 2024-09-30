a5dff5d2e7c4a71dbcac0b686c271b116ce6ee0d
davidrtfraser <david.rt.fraser@gmail.com>
2024-09-30 12:08:07 +0100

    Tracking all of Day 1 completed files

---
+++ services/trade_producer/src/kraken_websocket_api.py
@@ -0,0 +1,129 @@
+from typing import List
+from datetime import datetime, timezone
+from websocket import create_connection
+from loguru import logger
+from pydantic import BaseModel
+import json
+
+class Trade(BaseModel):
+    # We use pydantic to validate the data; this is a dataclass from pydantic
+    # BaseModel is a data class that we inherit from
+    product_id: str
+    quantity: float
+    price: float
+    timestamp_ms: int
+
+class KrakenWebsocketAPI:
+    """
+    Class for reading data from Kraken Websocket API
+    """
+    URL = 'wss://ws.kraken.com/v2'
+
+    def __init__(self, product_id: str):
+        """"
+        Initializes the KrakenWebsocketAPI instange
+        
+        Args:
+            product_id (str): The product id to get the trades from
+        """
+
+        self.product_id = product_id
+
+        # Establish connection to the Kraken Websocket API
+        self._ws = create_connection(self.URL)
+        logger.debug(f"Connection Established")
+
+        # Subscribe to the given trades for the given product_id
+        self._subscribe(product_id)
+
+    def get_trades(self)->List[Trade]:
+        """
+        Returns the latest batch of trades from the Kraken Websocket API
+
+        Args:
+            None
+        Returns:
+            List[Trade]: A list of trades
+        """
+        # Get the data from the websocket
+        message = self._ws.recv()
+        
+        if 'heartbeat' in message:
+            # When we receive a heartbeat, we return an empty list
+            # message that communicates connection is still alive
+            logger.debug('Received heartbeat')
+            return []
+        
+        # Parse the message as dictionary
+        message = json.loads(message)
+        
+        # Extract the data from the message
+        trades = []
+        for trade in message['data']:
+            # EXTRACT THE FOLLOWING FIELDS FROM THE TRADE
+            # -product_id
+            # -quantity
+            # -price
+            # -timestamp_ms
+
+            # We use pydandtic to validate the data
+            trades.append(
+                Trade(
+                    product_id=trade['symbol'],
+                    price=trade['price'],
+                    quantity=trade['qty'],
+                    timestamp_ms=self.to_ms(trade['timestamp']),
+                )
+            )
+        return trades 
+
+    def is_done(self)->bool:
+        """
+        Returns True if the Kraken Websocket API connection is closed
+        """
+        # False because we are streaming from a websocket; but if we were doing batches
+        # of trades, we would return True when we are done with last batch
+        False
+    
+    def _subscribe(self, product_id: str):
+        # Everything is public in python; but it is good practice to use _ for private methods; this is not
+        # supposed to be called from someone else outside of the class
+        """
+        Subscribes to the given product_ids
+        """
+        logger.info(f'Subscribing to the trades for {product_id}')
+        
+        # Subscribe to the given product_ids
+        msg =  {
+            "method": "subscribe",
+            "params": {
+                "symbol": [product_id],
+                "channel": "trade",
+                "snapshot": False,
+            },
+        }
+        self._ws.send(json.dumps(msg))
+        logger.info(f'Subscrition worked')
+
+        # For each product_id we dump
+        # the first 2 messages we got from the websocket because they contain
+        # no trade data, just confirmation of subscription
+        for product_id in [product_id]:
+            _ = self._ws.recv()
+            _ = self._ws.recv()
+    
+    # Static method because it does not depend on the instance of the class
+    # Decorators are used to modify the behavior of functions or methods
+    @staticmethod
+    def to_ms(timestamp: str)->int:
+        """
+        Converts a Kraken timestamp to Unix timestamp in milliseconds
+        Args:
+            timestamp (str): The timestamp to convert
+        Returns:
+            int: The Unix timestamp in milliseconds
+        """
+
+        # Convert the timestamp to Unix timestamp in milliseconds
+        timestamp = datetime.fromisoformat(timestamp[:-1]).replace(tzinfo=timezone.utc)
+        return int(timestamp.timestamp() * 1000)
---
+++ services/trade_producer/src/main.py
@@ -0,0 +1,56 @@
+from typing import List
+from quixstreams import Application
+from loguru import logger
+from src.kraken_websocket_api import KrakenWebsocketAPI, Trade
+
+
+def produce_trades(
+    kafka_broker_address: str,
+    kafka_topic: str,
+    product_id: str,
+):
+    """
+    Reads trades from the Kraken WEbsocket API and produces them to a Kafka topic.
+    Args:
+        kafka_broker_address (str): The address of the Kafka broker.
+        kafka_topic (str): The name of the Kafka topic to save the trades.
+        product_id (str): The product ID to get the trades from.
+    Returns:
+        None
+    """
+
+    # Create an application with Kafka configuration
+    app = Application(broker_address=kafka_broker_address)
+    # Define the Kafka topic with JSON serialization
+    topic = app.topic(name=kafka_topic, value_serializer='json')
+
+    # Create a Kraken API object
+    kraken_api = KrakenWebsocketAPI(product_id=product_id)
+
+    # Create a producer (helps save data to the topic)
+    with app.get_producer() as producer:
+        while True:
+            # We use type annotation when we define the variable
+            trades: List[Trade] = kraken_api.get_trades()
+
+            # Serialize the event using the defined topic
+            # Transform the event into a sequence of bytes
+            for trade in trades:
+                # The point of the key is to make sure that all trades of the same product go to the same partition
+                # This way, the order of the trades is preserved and we can get data in parallel
+                # We get horizontal scalability
+                # trade.model_dump() is a method that serializes the trade object into a dictionary
+                message = topic.serialize(key=trade.product_id, value=trade.model_dump())
+                # Produce a message to the topic
+                producer.produce(topic=topic.name, value=message.value, key=message.key)
+
+                logger.debug(f"Pushed trade to Kafka: {trade}") 
+
+
+if __name__ == '__main__':
+    produce_trades(
+        kafka_broker_address='localhost:19092',
+        kafka_topic='trades',
+        product_id='BTC/USD',
+    )
+
